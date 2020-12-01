a077af400eaff5ffbc6e8b12654fbfbf7cf045dd
Oren Kanner <oren.kanner@gsa.gov>
2020-12-01 12:36:19 -0500

    Add AssetsHelper and fix all the things

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
+//= link_tree ../../../node_modules/identity-style-guide/dist/assets/fonts
 //= link_tree ../../../node_modules/identity-style-guide/dist/assets/img
 //= link_directory ../stylesheets .css
---
+++ app/helpers/assets_helper.rb
@@ -0,0 +1,5 @@
+module AssetsHelper
+  def login_design_asset_path(path)
+    "identity-style-guide/dist/assets/#{path}"
+  end
+end
